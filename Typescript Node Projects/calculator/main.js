95ade87cc51ef1ea65c4312da90343e96d90952c
Kainatraza <syedakainatrazanaqvirazanaqvi@gmail.com>
2024-02-19 03:40:22 +0500

    upd

---
+++ Typescript Node Projects/calculator/main.js
@@ -0,0 +1,56 @@
+// import { rejects } from "assert";
+// import { error } from "console";
+// import { resolve } from "path";
+// console.log("your result is :", result)
+// } else {
+//     console.log("kindly entre valid input")
+// }
+// let myPromise =new Promise ((resolve,reject)=>{
+//     console.log("promise pending");
+//     setTimeout(()=>{
+//         // console.log("promise reject");
+//         // resolve(["bilal","khan"])
+//         console.log("promise reject");
+//         reject(new Error("data fetch failed"))
+//     },2000);
+// })
+// myPromise.then ((res)=>(console.log(res))).catch ((err)=>(console.log(err)))
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly entre your first number",
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "kindly entre your second number"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "select  Operator :",
+        choices: ["+", "-", "/", "*"]
+    },
+]);
+const { numberOne, numberTwo, operator } = answer;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("your result is:", result);
+}
+else {
+    console.log("Kindly entre valid input");
+}
